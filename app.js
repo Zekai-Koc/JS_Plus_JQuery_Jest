10e703a8d14c70e70d53183e79034bc886a3a09b
MA <victor@clarusway.com>
2021-08-31 20:04:49 +0300

    latest version.

--- app.js
+++ app.js
@@ -372,3 +372,177 @@
 
 
 // // ---------- Add/Remove Methods ------------ //
+// // now, let's add some new elements to our page. (html file.)
+
+
+// // ------------------ Append/Prepend ------------------ //
+
+// // we now add eventlisteners to our buttons.
+// $("#btnAppend").on("click", function () {
+//     // for creating an element in jQuery you can directly write inside of the paranthesis.
+//     var li = $("<li></li>");
+//     // if you like, you can add properties inside.
+//     // var li = $("<li class="item" id="jjj"><li/>");
+//     li.text($("#todoText").val());
+
+//     console.log(li);
+
+//     $("#todoList").append(li);
+// });
+
+
+// $("#btnPrepend").on("click", function () {
+//     var li = $("<li></li>");
+//     li.text($("#todoText").val());
+//     $("#todoList").prepend(li);
+// });
+
+
+// // ------------------ Before/After ------------------ //
+
+
+// $("#btnAddBefore").on("click", function () {
+//     var li = $("<li></li>");
+//     li.text($("#todoText").val());
+//     $("#todoList #item2").before(li); // here we select a specific item.
+// });
+
+// $("#btnAddAfter").on("click", function () {
+//     var li = $("<li></li>");
+//     li.text($("#todoText").val());
+//     $("#todoList #item2").after(li); // here we select a specific item.
+// });
+
+
+
+// // ------------------ Class&CSS Addings ------------------ //
+
+// $("#btnAddClass").on("click", function() {
+//     $("p").addClass("green red");
+// });
+
+// $("#btnRemoveClass").on("click", function() {
+//     $("p").removeClass("green red");
+// });
+
+// $("#btnToggleClass").on("click", function() {
+//     $("p").toggleClass("green red");
+// });
+
+// $("#btnCss").on("click", function() {
+//     // tek özellik ekleme.
+//     // $("p").css("color", "green");
+
+//     // multiple attribute...
+//     $("p").css({
+//         "color": "green",
+//         "fontStyle": "italic"
+//     });
+// });
+
+
+
+// // ------------------ Traversing ------------------ //
+
+// // here, we can access all the child of our list.
+// var ulChildren = $("#options").children();
+// // we can check what we have.
+// console.log(ulChildren);
+
+// // now, we will iterate through each element and ad "list-item" class one by one.
+// ulChildren.each(function (i, e) {
+//     $(e).addClass("list-item");
+// });
+
+// // here, we will catch "li" elements of "#options" id and ad click event for the first one.
+// $("#options li").eq(0).on("click", function() {
+//     $(this).toggleClass("bold");
+// });
+
+// $("#options li").eq(1).on("click", function() {
+//     $(this).toggleClass("italic");
+// });
+
+// $("#options li").eq(2).on("click", function() {
+//     $(this).toggleClass("underline");
+// });
+
+// $("#options li").eq(3).on("click", function() {
+//     $(this).toggleClass("color");
+// });
+
+// $("#options li").eq(4).on("click", function() {
+//     $(this).toggleClass("bg");
+// });
+
+// // find element with id "options", then go to its parent and find the first button on the parent element (in our case, body) then add click event...
+// $("#options").parent().find("button:first").on("click", function() {
+//     $("#options").find("li").attr("class", "list-item");
+// });
+
+// let elementAfterHr = $("hr").next();
+// console.log(elementAfterHr);
+
+// let allElementsAfterHr = $("hr").nextAll();
+// console.log(allElementsAfterHr);
+
+// let findBeforeBtn = $("hr").prev();
+// console.log(findBeforeBtn);
+
+// let findAllBeforeBtn = $("hr").prevAll();
+// console.log(findAllBeforeBtn);
+
+
+
+// // ------------------ Ajax Calls ------------------ //
+
+
+let dataLink = "https://jsonplaceholder.typicode.com/posts";
+
+// let ul = $("#list");
+
+// $("#btnLoadData").on("click", function () {
+
+//     $.ajax({
+//         method: "GET",
+//         url: dataLink
+//     }).done(function (data) {
+//         console.log(data);
+
+//         for (let index = 0; index < data.length; index++) {
+//             const elementLi = $("<li></li>");
+//             elementLi.text(data[index].title);
+//             ul.append(elementLi);
+//         }
+
+//     }).fail(function () {
+//         console.log("hata");
+//     })
+
+// });
+
+
+// now, the last thing that i will show you is: 
+// what will be the output if i execute the following code?
+
+console.log("before ajax call.");
+
+$.ajax({
+    method: "GET",
+    url: dataLink,
+    async: false
+}).done(function (data) {
+    console.log(data);
+}).fail(function () {
+    console.log("hata");
+});
+
+console.log("after ajax call.")
+
+
+
+
+
+
+
+
